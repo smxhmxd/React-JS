985b78c7fa7cb24e405c10f22568563b4f07c568
Ahmad <55134836+smxhmxd@users.noreply.github.com>
2024-04-16 23:50:41 +0530

    Update main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -20,25 +20,31 @@ function MyApp(){
 //     children: 'Click me to visit google'
 // }
 
+
+// The above will not run as it expects a format for the code to run
+// We can run the above code using the below format
+
 const anotherElement = (
-    <a href="https://google.com" target='_blank'>Visit google</a>
+    <a href="https://fast.com" target='_blank'> Visit google</a>
 )
 
 
 
-const anotherUser = "chai aur react"
+const anotherUser = " chai aur react" // JSX
 
 const reactElement = React.createElement(
     'a',
     {href: 'https://google.com',target: '_blank' },
     'click me to visit google',
+    anotherUser,  // Evaluated Expression
     anotherElement
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    reactElement
-  
+
+    reactElement // we are calling an object, not a function so () is not needed
+    // click me to visit google chai aur react(google.com) Visit google(fast.com)
+
 )
 
 
