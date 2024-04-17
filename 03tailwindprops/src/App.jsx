dabb91944be18686a7287b1a4c4abb6e1eb33760
Ahmad <55134836+smxhmxd@users.noreply.github.com>
2024-04-17 14:11:26 +0530

    React Tailwind Props 03
    Learned tailwind basics and props works in React

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="chaiaur" btnText="newArr" />
+      {/*We can pass value to components as Props*/}
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" />
+    </>
+  )
+}
+
+export default App
